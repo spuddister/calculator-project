f1eb3c1c6356d472e2f82badd0499c8017f9826a
Sam Puddister <sampuddister@gmail.com>
2022-01-27 16:05:59 -0500

    Added event listeners to all buttons except clear. Bottom display now updates with number inputs, however number limiting is not in place yet.

--- script.js
+++ script.js
@@ -1,3 +1,85 @@
+let prevVal = 0;
+let value = 0;
+let operator;
+
+//NEXT: when you're back code in the prevValue from the operator buttons!!
+
+//OPERATOR BUTTONS
+const equalsBtn = document.getElementById('equals').addEventListener('click', function(){
+    console.log('equals');
+    value = operate(prevVal, value, operator);
+});
+const addBtn = document.getElementById('add').addEventListener('click', function(){
+    console.log('add');
+    operator = 'add';
+})
+const subtractBtn = document.getElementById('subtract').addEventListener('click', function(){
+    console.log('subtract');
+    operator = 'subtract';
+})
+const multiplyBtn = document.getElementById('multiply').addEventListener('click', function(){
+    console.log('multiply');
+    operator = 'multiply';
+})
+const divideBtn = document.getElementById('divide').addEventListener('click', function(){
+    console.log('divide');
+    operator = 'divide';
+})
+const backBtn = document.getElementById('backspace').addEventListener('click', function(){
+    console.log('backspace');
+    botDisplayRemove();
+})
+
+//NUMBER BUTTONS
+const zero = document.getElementById('zero').addEventListener('click', function(){
+    botDisplayAdd(0);
+})
+const one = document.getElementById('one').addEventListener('click', function(){
+    botDisplayAdd(1);
+})
+const two = document.getElementById('two').addEventListener('click', function(){
+    botDisplayAdd(2);
+})
+const three = document.getElementById('three').addEventListener('click', function(){
+    botDisplayAdd(3);
+})
+const four = document.getElementById('four').addEventListener('click', function(){
+    botDisplayAdd(4);
+})
+const five = document.getElementById('five').addEventListener('click', function(){
+    botDisplayAdd(5);
+})
+const six = document.getElementById('six').addEventListener('click', function(){
+    botDisplayAdd(6);
+})
+const seven = document.getElementById('seven').addEventListener('click', function(){
+    botDisplayAdd(7);
+})
+const eight = document.getElementById('eight').addEventListener('click', function(){
+    botDisplayAdd(8);
+})
+const nine = document.getElementById('nine').addEventListener('click', function(){
+    botDisplayAdd(9);
+})
+
+//DISPLAYS
+const botDisplay = document.getElementById('input');
+
+//FUNCTIONS
+function botDisplayAdd(newDigit){
+    value = value*10 + newDigit;
+    botDisplay.textContent = value;
+}
+function botDisplayRemove(){
+    if (botDisplay.textContent.length > 1 && value > 1) {
+        value = Math.floor(value/10);
+        botDisplay.textContent = value;
+        console.log('here')
+    } else {
+        value = 0;
+        botDisplay.textContent = value;
+    } 
+}
 
 function add(x, y) {
     return x + y;
